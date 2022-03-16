2b998d70e1bc3b98a4a2888e10028ba635786b1d
GELRPT <gelr@premiertech.com>
2022-03-16 18:54:46 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("78f45347-4e1b-4104-97cf-c28154bfe3c0");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
